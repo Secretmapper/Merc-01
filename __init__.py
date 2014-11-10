4073430c64fef4ec42efa54a2de8ef4e79c01716
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2014-11-10 12:35:22 +0800

    Puts Spatial Grid on its own class

--- __init__.py
+++ __init__.py
@@ -62,27 +62,74 @@ class Camera(object):
 			0,self.win.width,
 			0,self.win.height)
 
+class Spatial_Grid():
+
+	def __init__(self):
+		self._cb_type_i = 0
+		self._cb_types = [] #cache _cb_types for hit list creation
+		self._entities = [] #memory intensive
+
+	def clear(self):
+		self._hit_squares = [[[[] for i in self._cb_types] for b in range(int(win_width/cell_size))] for a in range(int(win_height/cell_size))]
+
+	def update(self):
+		for col in self._hit_squares:
+			for cell in col:
+				a_list = cell[0]
+				b_list = cell[1]
+				for a in a_list:
+					for b in b_list:
+						if not (a.dead or b.dead) and utils.distance_sq(b, a) < (a.width/2 + b.width/2)**2:
+							a.dead = True
+							b.dead = True
+
+	def add_entity(self, e, i):
+		self._entities[i].append(e)
+
+	def remove_entity(self, e, i):
+		self._entities[i].remove(e) #warning: O(n)
+
+	def add(self, item, i):
+		col, cell, col_e, cell_e = map(lambda x: int(x/cell_size), item.pos_vertices())
+		for a in range(col, col_e+1):
+			for b in range(cell, cell_e+1):
+				if a < max_cell_h and b < max_cell_w and a >= 0 and b >= 0:
+					self._hit_squares[a][b][i].append(item)
+
+	def cb_type(self):
+		self._cb_type_i += 1
+		self._cb_types.append(0)
+		self._entities.append([])
+		return self._cb_type_i - 1
+
 
 class Game_Window(pyglet.window.Window):
 
 	def __init__(self, width, height):
 		super(Game_Window, self).__init__(width, height)
 		self.main_batch = game.graphics.Layer()
+
+		self.camera = Camera(self, zoom=500.0)
+		self.camera.track(self.main_batch)
+		self.camera.shake(50)
+
+		self.spatial_grid = Spatial_Grid()
+		self.BULLET_CB_TYPE = self.spatial_grid.cb_type()
+		self.ENEMY_CB_TYPE = self.spatial_grid.cb_type()
+
 		self.ship = Ship(img=res.player, x=400, y=300, batch=self.main_batch)
 		self.bullets = []
-
 		self.enemies = []
+
 		for i in range(300):
-			self.enemies.append(EnemyShip(img=res.player, x=randint(50,self.width-50), y=randint(50,self.height-50), batch=self.main_batch))
+			enemy = EnemyShip(img=res.player, x=randint(50,self.width-50), y=randint(50,self.height-50), batch=self.main_batch)
+			self.spatial_grid.add_entity(enemy, self.ENEMY_CB_TYPE)
+			self.enemies.append(enemy)
 		
 		pyglet.clock.set_fps_limit(60)
 		pyglet.clock.schedule(self.on_update)
 		
 		self.push_handlers(self.ship)
-
-		self.camera = Camera(self, zoom=500.0)
-		self.camera.track(self.main_batch)
-		self.camera.shake(50)
 
 	def on_mouse_motion(self, x, y, dx, dy):
 		self.last_mouse_pos = (x, y)
@@ -90,6 +137,7 @@ class Game_Window(pyglet.window.Window):
 	def on_update(self, dt):
 		if self.ship.shoot:
 			bullet = Bullet(on_bounds_kill=False, img=res.bullet, x=self.ship.x, y=self.ship.y, r=self.ship.rotation, batch=self.main_batch)
+			self.spatial_grid.add_entity(bullet, self.BULLET_CB_TYPE)
 			self.bullets.append(bullet)
 		self.ship.update(dt)
 
@@ -100,40 +148,25 @@ class Game_Window(pyglet.window.Window):
 
 		for bullet in [b for b in self.bullets if b.dead]:
 			self.bullets.remove(bullet)
+			self.spatial_grid.remove_entity(bullet, self.BULLET_CB_TYPE)
 
 		for enemy in [b for b in self.enemies if b.dead]:
 			self.enemies.remove(enemy)
-		
-		hit_squares = [[[[],[]] for b in range(int(win_width/cell_size))] for a in range(int(win_height/cell_size))]
+			self.spatial_grid.remove_entity(enemy, self.ENEMY_CB_TYPE)
 
+		self.spatial_grid.clear()
+		
 		for bullet in self.bullets:
-			col, cell, col_e, cell_e = map(lambda x: int(x/cell_size), bullet.pos_vertices())
-			for a in range(col, col_e+1):
-				for b in range(cell, cell_e+1):
-					if a < max_cell_h and b < max_cell_w and a >= 0 and b >= 0:
-						hit_squares[a][b][1].append(bullet)
 			bullet.update(dt)
+			self.spatial_grid.add(bullet, self.BULLET_CB_TYPE)
 
 		for enemy in self.enemies:
-			col, cell, col_e, cell_e = map(lambda x: int(x/cell_size), enemy.pos_vertices())
-			for a in range(col, col_e+1):
-				for b in range(cell, cell_e+1):
-					if a < max_cell_h and b < max_cell_w and a >= 0 and b >= 0:
-						hit_squares[a][b][0].append(enemy)
 			enemy.update(dt)
+			self.spatial_grid.add(enemy, self.ENEMY_CB_TYPE)
 
-		for col in hit_squares:
-			for cell in col:
-				enemies = cell[0]
-				bullets = cell[1]
-				for a in enemies:
-					for b in bullets:
-						if not (a.dead or b.dead) and utils.distance_sq(b, a) < (a.width/2 + b.width/2)**2:
-							a.dead = True
-							b.dead = True
+		self.spatial_grid.update()
 
 	def on_draw(self, ):
-		#self.clear()
 		glClear(GL_COLOR_BUFFER_BIT)
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
