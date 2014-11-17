54161d29f1e376d41664a30469b1f940c8ed3f3f
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2014-11-17 12:22:35 +0800

    Moduled Camera

--- __init__.py
+++ __init__.py
@@ -10,53 +10,6 @@ import math
 
 fps_display = pyglet.clock.ClockDisplay()
 
-class Camera(object):
-
-	def __init__(self, win, x=0, y=0, z=0, zoom=1.0):
-		self.win = win
-		self.zoom = zoom
-		self.x, self.y, self.z = x, y, z
-		
-		self._offset = [0, 0]
-		self._track = type('obj', (object,), {'x':0, 'y':0})
-		self._radius = 0
-		self._shake_dec = 0 #how much the shake dies down every update
-
-	def update(self, dt):
-		if self._radius > 0:
-			self._radius *= self._shake_dec
-			self._angle += 180 if randint(0, 1) == 0 else 60 
-			self._offset = [math.sin(self._angle) * self._radius , math.cos(self._angle) * self._radius]
-			if math.floor(self._radius) == 0.0: self._radius = 0
-
-		self.x = ( self._track.x + self._offset[1] )/float(CONSTS.win_width/2)
-		self.y = ( self._track.y + self._offset[0] )/float(CONSTS.win_height/2)
-
-	def track(self, track):
-		self._track = track
-
-	def shake(self, r, dec= 0.9):
-		self._radius = r
-		self._shake_dec = dec
-		self._angle = randint(0, 360) 
-		self._offset = [ math.sin(self._angle) * self._radius , math.cos(self._angle) * self._radius]
-
-	def game_projection(self):
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		glTranslatef(self.x, self.y, self.z)
-		gluOrtho2D( 
-			0,self.win.width,
-			0,self.win.height)
-
-	def hud_projection(self):
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		#glTranslatef(self.x, self.y, self.z)
-		gluOrtho2D( 
-			0,self.win.width,
-			0,self.win.height)
-
 class Spatial_Grid():
 
 	def __init__(self):
@@ -106,7 +59,7 @@ class Game_Window(pyglet.window.Window):
 		super(Game_Window, self).__init__(width, height)
 		self.main_batch = game.graphics.Layer()
 
-		self.camera = Camera(self, zoom=500.0)
+		self.camera = game.graphics.Camera(self, zoom=500.0)
 		self.camera.track(self.main_batch)
 		self.camera.shake(50)
 
